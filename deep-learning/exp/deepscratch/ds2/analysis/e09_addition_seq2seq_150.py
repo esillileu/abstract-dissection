705aaa848fd5d8b046eccceb20460c4dbefb27fe
esillileu <esillileu@gmail.com>
2026-08-14 15:09:00 +0900

    update: ds09 filename

--- deep-learning/exp/deepscratch/ds2/analysis/e09_addition_seq2seq_150.py
+++ deep-learning/exp/deepscratch/ds2/analysis/e09_addition_seq2seq_150.py
@@ -24,37 +24,37 @@ DEFINITIONS = (
 
 ADDITIONAL_GRAPHS = (
     (
-        "ds2_e09_van_fwd_vs_van_rev.png",
+        "ds2_e09_01_van_fwd_vs_van_rev.png",
         "Vanilla Seq2seq: Forward vs. Reverse",
         ("SEQA-VAN-FWD", "SEQA-VAN-REV"),
         "upper left",
     ),
     (
-        "ds2_e09_pky_fwd_vs_pky_rev.png",
+        "ds2_e09_01_pky_fwd_vs_pky_rev.png",
         "Peeky Seq2seq: Forward vs. Reverse",
         ("SEQA-PEEKY-FWD", "SEQA-PEEKY-REV"),
         "upper left",
     ),
     (
-        "ds2_e09_atn_fwd_vs_atn_rev.png",
+        "ds2_e09_01_atn_fwd_vs_atn_rev.png",
         "Attention Seq2seq: Forward vs. Reverse",
         ("SEQA-ATTN-FWD", "SEQA-ATTN-REV"),
         "upper left",
     ),
     (
-        "ds2_e09_atn_pky_fwd_vs_atn_pky_rev.png",
+        "ds2_e09_01_atn_pky_fwd_vs_atn_pky_rev.png",
         "Attention + Peeky Seq2seq: Forward vs. Reverse",
         ("SEQA-ATTN-PEEKY-FWD", "SEQA-ATTN-PEEKY-REV"),
         "upper left",
     ),
     (
-        "ds2_e09_pky_rev_vs_atn_pky_rev.png",
+        "ds2_e09_02_pky_rev_vs_atn_pky_rev.png",
         "Does Attention Help on Top of Peeky + Reverse?",
         ("SEQA-PEEKY-REV", "SEQA-ATTN-PEEKY-REV"),
         "lower right",
     ),
     (
-        "ds2_e09_van_rev_vs_atn_rev.png",
+        "ds2_e09_02_van_rev_vs_atn_rev.png",
         "Vanilla + Reverse vs. Attention + Reverse",
         ("SEQA-VAN-REV", "SEQA-ATTN-REV"),
         "lower right",
